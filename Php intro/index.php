4ab474851a8fcbe17f4aea2068893c23d11d888e
rsm5209 <60585354+rsm5209@users.noreply.github.com>
2020-05-13 12:21:28 +1200

    Create index.php

---
+++ Php intro/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+      // We use echo to create HTML (print)
+      echo("<h1>Hello World</h1>");
+
+      // Variables always start as $
+      $name = "Mickey Mouse";
+
+      // Variables can be inserted into strings, such as when we are echoing something
+      echo("Hello $name");
+
+     ?>
+  </body>
+</html>
