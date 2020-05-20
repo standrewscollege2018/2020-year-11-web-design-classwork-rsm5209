888b91ab54ed83e8273b725f607ee7860a504bcf
rsm5209 <60585354+rsm5209@users.noreply.github.com>
2020-05-21 11:33:31 +1200

    a LITTLERE

--- Php intro/index.php
+++ Php intro/index.php
@@ -11,10 +11,23 @@
       echo("<h1>Hello World</h1>");
 
       // Variables always start as $
-      $name = "Mickey Mouse";
+      $name = "Donald Mouse";
 
       // Variables can be inserted into strings, such as when we are echoing something
       echo("Hello $name");
+
+      // if statement use () brackets for the condition and {} for the code to run
+      if ($name == "Mickey Mouse") {
+        echo("Kill Donald");
+      } else {
+        echo("Kill Mickey");
+      }
+
+      $counter = 1;
+      do {
+        echo($counter);
+        $counter += 1;
+      } while ($counter <= 10);
 
      ?>
   </body>
